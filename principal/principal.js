3f75e95253d681aa82ba6c6025aaa3a78a01a0a4
miguelsf12 <87654640+miguelsf12@users.noreply.github.com>
2023-04-21 20:24:09 -0300

    atualizado

---
+++ principal/principal.js
@@ -0,0 +1,36 @@
+const btnAbrirMenu = document.querySelector('.abrir-menu-mb')
+const btnFecharMenu = document.querySelector('.fechar-menu-mb')
+const caixaMenu = document.querySelector('.header-mobile-itens')
+
+let show = true
+
+// SUMINDO ELEMENTOS AO ABRIR MENU
+const btnDuvidas = document.querySelector('.btnDuvidas')
+const btnScrool = document.querySelector('.btnScroll')
+const header = document.querySelector('.header-mobile')
+const main = document.querySelector('main')
+
+btnAbrirMenu.addEventListener('click', () => {
+    if (caixaMenu.style.display == 'flex') {
+        caixaMenu.style.display = 'none';
+        document.body.style.overflow = "initial"
+        btnDuvidas.style.display = 'block';
+        btnScrool.style.display = 'block';
+
+        
+        header.style.display = 'block'
+    } else {
+        caixaMenu.style.display = 'flex';
+        document.body.style.overflow = "hidden"
+        btnDuvidas.style.display = 'none';
+        btnScrool.style.display = 'none';
+
+        
+        header.style.display = 'none'
+    }
+})
+
+btnAbrirMenu.onclick = () => {
+    btnAbrirMenu.classList.toggle('ativo')
+}
+
